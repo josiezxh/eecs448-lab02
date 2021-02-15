81b86e13eb785e7e127803d6c2a4cc4e46e8be20
josiezxh <josiejharding@gmail.com>
2021-02-14 21:18:47 -0600

    search method defined.

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -38,9 +38,16 @@ bool LinkedList<T>::search(T value) const
 	Node<T>* temp = m_front;
 	bool isFound = false;
 
-	/** TODO 
-		Fix this method
-	*/
+	for(int i=1; i <= m_size; i++)
+	{
+		if(temp->getValue() == T)
+		{
+			ifFound = true;
+			break;
+		}
+		
+		temp = temp->getNext();
+	}
 
 	return(isFound);
 }
